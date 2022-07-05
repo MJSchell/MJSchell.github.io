8073c9a11ba6aa8be6f276ddb6ac1ceb515f8de0
M Schel <scheller.mason180@gmail.com>
2022-07-05 14:33:12 -0500

    m1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-$("#portbutton").click(function(){
-    $("#portfolioholder").scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-    });
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 600) {
+        $('#header').css("background-color","#3a3838");    
+    } else {
+        $('#header').css("background-color","transparent");
+    }
 });
