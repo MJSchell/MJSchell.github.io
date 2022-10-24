38dcc89ea55ef958ede29f7b98aa4ca221e61f6c
M Schel <scheller.mason180@gmail.com>
2022-10-23 23:03:04 -0500

    .

--- Portfolio/script.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 200) {
-        $('#header').css("background-color","#3a3838");    
-    } else {
-        $('#header').css("background-color","transparent");
-    }
-});
